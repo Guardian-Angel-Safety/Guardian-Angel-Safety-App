da617c8c0b66d93fbca06a80fb2b4511936ec769
Jeremy Eudy <jeremyeudy@gmail.com>
2019-02-07 10:43:20 -0500

    Finished data prototype.

--- app/src/main/java/com/example/guardianangelsafetyapp/DataActivity.java
+++ app/src/main/java/com/example/guardianangelsafetyapp/DataActivity.java
@@ -1,57 +1,56 @@
 package com.example.guardianangelsafetyapp;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.Button;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
 import java.util.Random;
-
-import org.achartengine.ChartFactory;
-import org.achartengine.GraphicalView;
-import org.achartengine.chart.BarChart;
-import org.achartengine.chart.PointStyle;
-import org.achartengine.model.XYMultipleSeriesDataset;
-import org.achartengine.model.XYSeries;
-import org.achartengine.renderer.XYMultipleSeriesRenderer;
-import org.achartengine.renderer.XYSeriesRenderer;
 
 public class DataActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.data_activity);
-        XYSeries series = new XYSeries("Temperature Data Feed");
+
+        final Button refresh = findViewById(R.id.refresh);
+        final GraphView graph = DrawGraph();
+
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RefreshGraph(graph);
+                DrawGraph();
+            }
+        });
+
+    }
+
+    protected GraphView DrawGraph(){
+        GraphView graph = (GraphView) findViewById(R.id.graph);
         Random r = new Random();
-        int data;
         int from = 75;
         int to = 90;
-        for(int i = 0; i<100; i++){
-            data = from + r.nextInt(to - from + 1);
-            series.add(i, data);
-        }
-        XYSeriesRenderer renderer = new XYSeriesRenderer();
-        renderer.setLineWidth(2);
-        renderer.setColor(Color.BLUE);
-        renderer.setDisplayBoundingPoints(true);
-        renderer.setPointStyle(PointStyle.CIRCLE);
-        renderer.setPointStrokeWidth(3);
+        ArrayList<DataPoint> points = new ArrayList();
+        //Bluetooth code will go here:
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, from + r.nextInt(to - from + 1)),
+                new DataPoint(1, from + r.nextInt(to - from + 1)),
+                new DataPoint(2, from + r.nextInt(to - from + 1)),
+                new DataPoint(3, from + r.nextInt(to - from + 1)),
+                new DataPoint(4, from + r.nextInt(to - from + 1)),
+        });
+        graph.addSeries(series);
+        return graph;
+    }
 
-        XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
-        mRenderer.addSeriesRenderer(renderer);
-        mRenderer.setMarginsColor(Color.argb(0x00, 0xff, 0x00, 0x00));
-        mRenderer.setPanEnabled(false, false);
-        mRenderer.setYAxisMax(35);
-        mRenderer.setYAxisMin(0);
-        mRenderer.setShowGrid(true);
-
-        LinearLayout chartContainer = (LinearLayout) findViewById(R.id.chart);
-
-        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-        dataset.addSeries(series);
-        chartContainer.removeAllViews();
-        GraphicalView chart = ChartFactory.getBarChartView(getParent(), dataset, mRenderer, BarChart.Type.DEFAULT);
-        chartContainer.addView(chart);
+    protected void RefreshGraph(GraphView graph){
+        graph.removeAllSeries();
     }
 
 }

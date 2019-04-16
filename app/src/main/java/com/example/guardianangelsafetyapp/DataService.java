6065867d172a2c4360757b0d05427b756cf9b861
Jeremy Eudy <jeremyeudy@gmail.com>
2019-04-16 14:54:55 -0400

    Made progress on network service

--- app/src/main/java/com/example/guardianangelsafetyapp/DataService.java
+++ app/src/main/java/com/example/guardianangelsafetyapp/DataService.java
@@ -4,8 +4,25 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-public class DataService extends Service {
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+
+public class DataService extends Service {
+    String link = "https://google.com";
+    URL url = new URL(link);
+
+    public DataService() throws MalformedURLException {
+    }
 
     @Override
     public IBinder onBind(Intent arg0){
@@ -20,6 +37,11 @@ public class DataService extends Service {
     @Override
     public void onStart(Intent intent, int startId){
         super.onStart(intent, startId);
+        try {
+            pushData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -27,4 +49,25 @@ public class DataService extends Service {
         super.onDestroy();
     }
 
+    public void pushData() throws IOException {
+        while(true) {
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            try {
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                readStream(in);
+            } finally {
+                urlConnection.disconnect();
+            }
+        }
+    }
+
+    private String readStream(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
+        for (String line = r.readLine(); line != null; line =r.readLine()){
+            sb.append(line);
+        }
+        is.close();
+        return sb.toString();
+    }
 }

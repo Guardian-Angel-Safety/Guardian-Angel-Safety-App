47e493d89515a00a3b9a033b1172f83bb40fa661
Jeremy Eudy <jeremyeudy@gmail.com>
2019-04-15 20:08:12 -0400

    Added frameworks for the DataReceiver and DataService classes.

---
+++ app/src/main/java/com/example/guardianangelsafetyapp/DataReceiver.java
@@ -0,0 +1,13 @@
+package com.example.guardianangelsafetyapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DataReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        //Pass data through to MainActivity and DataActivity here
+    }
+}
--- app/src/main/java/com/example/guardianangelsafetyapp/DataService.java
+++ app/src/main/java/com/example/guardianangelsafetyapp/DataService.java
@@ -1,15 +1,11 @@
 package com.example.guardianangelsafetyapp;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.IBinder;
 
 public class DataService extends Service {
-    
+
 
     @Override
     public IBinder onBind(Intent arg0){
